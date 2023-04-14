20344b699587a68bf9858960d6bdf7f883751c25
VuDInhPhong <vudinhphong.261001@gmail.com>
2023-04-14 11:52:46 +0700

    [CRUD] Create new course

--- src/app/controllers/CoursesController.js
+++ src/app/controllers/CoursesController.js
@@ -1,5 +1,4 @@
 const Course = require('../models/Course');
-const { mutipleMongooseToObject } = require('../../util/mongooes');
 const { mongooseToObject } = require('../../util/mongooes');
 class CoursesController {
     show(req, res, next) {
@@ -11,6 +10,16 @@ class CoursesController {
                 }),
             )
             .catch(next);
+    }
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = "https://scontent.fsgn5-8.fna.fbcdn.net/v/t1.15752-9/338791106_506774264814192_709385401465265985_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=ae9488&_nc_ohc=Tr5PJ9-URTcAX_WtBi_&_nc_ht=scontent.fsgn5-8.fna&oh=03_AdSNgg3fP79R6CE0TDYnKh9afU3rtUYCzhBjFpQYNz3MOg&oe=64603B09"
+        const course = new Course(formData);
+        course.save();
+        res.send('Course save');
     }
 }
 module.exports = new CoursesController;
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
-
+const slug = require('mongoose-slug-generator');
 const Schema = mongoose.Schema;
-
+mongoose.plugin(slug);
 const Coure = new Schema({
     name: { type: String, maxLength: 255 },
     description: { type: String, maxLength: 255 },
-    image: { type: String, maxLength: 255 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now }
-})
+    image: { type: String },
+    slug: { type: String, slug: 'name', unique: true },
+    v: { type: String, maxLength: 255 },
+}, {
+    timestamps: true,
+});
+
 module.exports = mongoose.model('Course', Coure);
